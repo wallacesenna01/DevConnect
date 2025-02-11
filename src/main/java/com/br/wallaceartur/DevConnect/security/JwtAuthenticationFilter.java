9474939234c7c26dcf636465102dba6a0af4473f
Wallace Artur <wallacearthur_3@hotmail.com>
2025-02-11 16:35:14 -0300

    JwtAuthenticationfilter

---
+++ src/main/java/com/br/wallaceartur/DevConnect/security/JwtAuthenticationFilter.java
@@ -0,0 +1,56 @@
+package com.br.wallaceartur.DevConnect.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.impl.ClaimsHolder;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebFilter("/*")
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = extracToken(request);
+        if(token != null) {
+
+            // decodifica o token e obtem o username
+            DecodedJWT decodedJWT = JWT.decode(token);
+            String username = decodedJWT.getSubject();
+
+            // cria autenticao do usuario
+            if(username != null && jwtUtil.validateToken(token, username)) {
+                SecurityContextHolder.
+                        getContext().
+                        setAuthentication(new UsernamePasswordAuthenticationToken(username,null, new ArrayList<>()));
+            }
+        }
+    }
+
+
+    private String extracToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if (header != null && header.startsWith("Bearer ")) {
+            return header.substring(7); // remove o Bearer do token
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/br/wallaceartur/DevConnect/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.br.wallaceartur.DevConnect.security;
+
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.AbstractConfiguredSecurityBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class).build();
+    }
+
+    
+
+}
